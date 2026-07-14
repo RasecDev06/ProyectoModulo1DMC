724cd4c44362409a2e1646126aa109072130ba64
RasecDev06 <156612837+RasecDev06@users.noreply.github.com>
2026-07-14 10:31:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,7 @@ if modulo == "Modulo listas":
 
 valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
 valor_final = st.number_input("Ingrese el valor final", value=1)
-
 lista_numerica = list(range(valor_inicial, valor_final))
-
 st.write(lista_numerica)
 
 elif modulo == "Modulo array":
