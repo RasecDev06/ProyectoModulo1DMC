6ab681f47d8229aa155145b6499315c242066090
RasecDev06 <156612837+RasecDev06@users.noreply.github.com>
2026-07-14 10:29:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,19 @@ st.sidebar.title("Parametros")
 st.image("Python_logo.png")
 st.sidebar.image("DMC.png")
 
+modulo = st.sidebar.selectbox("Elija un modulo", ["Modulo listas", "Modulo array", "Modulo funciones"])
+
+if modulo == "Modulo listas":
+
 valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
 valor_final = st.number_input("Ingrese el valor final", value=1)
 
 lista_numerica = list(range(valor_inicial, valor_final))
 
 st.write(lista_numerica)
+
+elif modulo == "Modulo array":
+    st.write("Estas en el modulo de arreglos")
+
+else 
+    st.write("Estas en el modulo de funciones")
