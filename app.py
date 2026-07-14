a97af7f158aec0240e02bffd0de097212a2c955f
RasecDev06 <156612837+RasecDev06@users.noreply.github.com>
2026-07-14 10:22:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ st.title("Proyecto modulo 1 fundamentals")
 st.sidebar.title("Parametros")
 
 st.image("Python_logo.png")
+st.sidebar.image("DMC.png")
 
 valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
 valor_final = st.number_input("Ingrese el valor final", value=1)
