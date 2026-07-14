941e3205523d3e65b385d56f2c6d9d856dbfa23a
RasecDev06 <156612837+RasecDev06@users.noreply.github.com>
2026-07-14 10:32:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ modulo = st.sidebar.selectbox("Elija un modulo", ["Modulo listas", "Modulo array
 if modulo == "Modulo listas":
 
 valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
-valor_final = st.number_input("Ingrese el valor final", value=1)
+valor_final = st.number_input("Ingrese el valor final", value-1)
 lista_numerica = list(range(valor_inicial, valor_final))
 st.write(lista_numerica)
 
