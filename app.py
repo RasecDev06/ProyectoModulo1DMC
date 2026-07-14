00e958f2deb1df5a13140fcc4b6ad1238f563322
RasecDev06 <156612837+RasecDev06@users.noreply.github.com>
2026-07-14 10:20:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 st.title("Proyecto modulo 1 fundamentals")
 st.sidebar.title("Parametros")
 
+st.image("Python_logo.png")
+
 valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
 valor_final = st.number_input("Ingrese el valor final", value=1)
 
